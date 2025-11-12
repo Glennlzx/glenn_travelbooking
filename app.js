56da5e82e14c22b444ef5e161a5af6f03fba944d
Glennlzx <glennzixin@gmail.com>
2025-11-12 11:43:04 +0800

    add app.js for running the app and rename node nodemodule file

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Starting my Node JS Project");
+console.log("Travel Booking App");
+const travel = require("./Glenn_TravelBooking.js");
+// console.log("Calculator's Description: " + calculator.description);
+// console.log("Perform add: " + calculator.add(2, 3));
+// console.log("Perform multiply: " + calculator.multiply(2, 3));
+// console.log("Perform subtract: " + calculator.subtract(2, 3));
+// console.log("Perform divide: " + calculator.divide(2, 3));
+
