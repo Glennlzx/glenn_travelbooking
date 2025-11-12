06a2656b56f3bcb1840d50be116cc4e7c6c341ea
Glennlzx <glennzixin@gmail.com>
2025-11-12 12:48:58 +0800

    3 functionalities done

--- Glenn_TravelBooking.js
+++ Glenn_TravelBooking.js
@@ -1,10 +1,27 @@
+//dummy data for simulating database
+let experiences = [
+    { name: "Tokyo 8 Day Tour", category: "Tour", location: "Tokyo", price: 4000 },
+    { name: "Osaka 7 Day Tour", category: "Tour", location: "Osaka", price: 3000 },
+    { name: "Universal Studio Japan one day ticket + express full package", category: "Attraction", location: "Osaka", price: 299 },
+]
 module.exports = {
-    // Explain what function A does
-    functionA() {
-        return 1+2;
+    // List all experiences from the "database"
+    listExperiences() {
+        return experiences;
     },
-    // Explain what function B does
-    functionB() {
-        console.log("Hello function B");
+    // Add new experience into the array (add)
+    addExperience(name, category, location, price) {
+        experiences.push({ name, category, location, price });
+        return `${name} has been added successfully`;
+    },
+    //Find experiences via location (filtering)
+    findByLocation(location) {
+        const results = experiences.filter(x => x.location.toLowerCase() === location.toLowerCase());
+        return results.length ? results : "No experiences found for this location.";
+    },
+    //update price for experience
+    updateExPrice(name, newPrice) {
+
     }
+
 }
--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 console.log("Starting my Node JS Project");
 console.log("Travel Booking App");
 const travel = require("./Glenn_TravelBooking.js");
-// console.log("Calculator's Description: " + calculator.description);
-// console.log("Perform add: " + calculator.add(2, 3));
-// console.log("Perform multiply: " + calculator.multiply(2, 3));
-// console.log("Perform subtract: " + calculator.subtract(2, 3));
-// console.log("Perform divide: " + calculator.divide(2, 3));
-
+console.log("Full experience list: ", travel.listExperiences());
+console.log(travel.addExperience("Shinkansen One Way Tokyo to Osaka", "Train Ticket", "Tokyo Station", 200));
+console.log(travel.findByLocation("Tokyo"))
